9bc76bf908b069f482d7861a3f16e1e13ef3d7e2
Petr Pechkurov <petr.pechkurov@gmail.com>
2022-11-20 23:12:09 +0300

    docker and sc

---
+++ asdf.sh
@@ -0,0 +1,55 @@
+# install nix
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+# source nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+  nixpkgs.zsh \
+  nixpkgs.git \
+  nixpkgs.stow \
+  nixpkgs.fzf \
+  nixpkgs.ripgrep \
+  nixpkgs.bat \
+  nixpkgs.cmake \
+  nixpkgs.lazygit \
+  nixpkgs.exa
+
+if [ $? -eq 0 ]; then
+  echo OK
+else
+  echo FAIL
+  exit 1
+fi
+
+# add zsh as a login shell
+command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell
+sudo chsh -s $(which zsh) $USER
+
+# stow dotfiles
+stow git
+stow p10k
+stow zsh
+stow alacritty
+
+exec zsh
+
+# zsh plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+
+# install ohmhzsh
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+nvm install node
+nvm install-latest-npm
