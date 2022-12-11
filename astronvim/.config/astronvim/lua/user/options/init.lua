a5d46077840579b2f591dc5b3ee718b3c414220e
Petr Pechkurov <petr.pechkurov@gmail.com>
2022-12-11 09:54:22 +0300

    wezter tweaks 1

--- astronvim/.config/astronvim/lua/user/options/init.lua
+++ astronvim/.config/astronvim/lua/user/options/init.lua
@@ -11,4 +11,15 @@ return {
 
 	gitblame_date_format = "%r",
 	gitblame_message_template = "<author> • <date> • <summary>",
+
+	neovide_floating_blur_amount_x = 2.0,
+	neovide_floating_blur_amount_y = 2.0,
+	neovide_fullscreen = false,
+	neovide_remember_window_size = true,
+	neovide_profiler = false,
+	neovide_cursor_animation_length = 0.05,
+	neovide_cursor_trail_length = 0.01,
+	neovide_cursor_trail_size = 0.01,
+	neovide_cursor_antialiasing = true,
+	neovide_cursor_vfx_mode = "railgun",
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,15 +3,37 @@ return {
   font = wezterm.font("CaskaydiaCove Nerd Font"),
   font_size = 18,
   color_scheme = "Gruvbox Dark",
-  default_cwd = "/home/pp/projects/",
-  keys = { { key = "z", mods = "ALT", action = wezterm.action.ShowLauncher } },
-  window_background_opacity = 0.85,
   enable_scroll_bar = true,
-  default_prog = { "zsh" },
+  audible_bell = "Disabled",
+
+  default_cwd = "/home/pp/projects/",
+
+  window_close_confirmation = "NeverPrompt",
+  window_background_opacity = 0.95,
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
+  },
+
+  -- tab bar
+  hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = true,
+  switch_to_last_active_tab_when_closing_tab = true,
+
+  -- keys
+  keys = {
+    { key = "z", mods = "ALT", action = wezterm.action.ShowLauncher },
+    { key = "h", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "w", mods = "CTRL", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+  },
+
+  -- inactive pane hsb
+  inactive_pane_hsb = {
+    saturation = 0.8,
+    brightness = 0.4,
   },
 }
