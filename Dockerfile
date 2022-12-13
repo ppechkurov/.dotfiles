abcd590dea3f7b5dc3d48383ed06f2724fde659c
Petr Pechkurov <petr.pechkurov@gmail.com>
2022-12-13 17:40:10 +0300

    update install.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu
 RUN apt-get update 
-RUN apt-get install -y curl sudo git vim xz-utils x11-xkb-utils x11-xserver-utils
+RUN apt-get install -y curl sudo git vim xz-utils x11-xkb-utils x11-xserver-utils unzip
 RUN useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1001 ubuntu
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 RUN echo 'ubuntu:ubuntu' | chpasswd
@@ -9,3 +9,4 @@ USER ubuntu
 ENV USER ubuntu
 WORKDIR /home/ubuntu
 COPY . /home/ubuntu/.dotfiles
+RUN echo "ubuntu" | sudo -S chown ubuntu /home/ubuntu/.dotfiles
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 set -e
+
 # install nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 echo '======> nix installed'
@@ -22,11 +23,14 @@ nix-env -iA \
 
 echo '======> nix packages installed'
 
+curl -LO https://github.com/wez/wezterm/releases/download/20221119-145034-49b9839f/wezterm-20221119-145034-49b9839f.Ubuntu22.04.deb
+sudo apt install -y ./wezterm-20221119-145034-49b9839f.Ubuntu22.04.deb
+
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
 
 # use zsh as default shell
-sudo chsh -s $(which zsh) $USER
+sudo chsh -s "$(which zsh)" "$USER"
 echo '======> zsh as default shell'
 
 # install ohmhzsh
@@ -34,13 +38,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo '======> oh-my-zsh installed'
 
 # zsh plugins
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-completions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
+git clone https://github.com/lukechilds/zsh-nvm "$HOME/.oh-my-zsh/custom/plugins/zsh-nvm"
 echo '======> plugins installed'
 
 rm -rf ~/.zshrc
@@ -49,8 +53,9 @@ rm -rf ~/.zshrc
 stow git
 stow p10k
 stow zsh
-stow alacritty
+# stow alacritty
 stow astronvim
+stow wezterm
 echo '======> stowed'
 
 # node
