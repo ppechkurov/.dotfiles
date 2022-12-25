eec536a05c63bb32c9a72c214455ee22fc6c9268
Petr Pechkurov <petr.pechkurov@gmail.com>
2022-12-25 17:50:51 +0300

    neotree

--- nvim/.config/nvim/lua/ppechkurov/packer.lua
+++ nvim/.config/nvim/lua/ppechkurov/packer.lua
@@ -77,6 +77,18 @@ return require("packer").startup(function(use)
 		},
 	})
 
+	-- Unless you are still migrating, remove the deprecated commands from v1.x
+	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+	use({
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	})
 	-- use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } })
 	use({ "jose-elias-alvarez/null-ls.nvim", "jayp0521/mason-null-ls.nvim" })
 end)
--- nvim/.config/nvim/lua/ppechkurov/set.lua
+++ nvim/.config/nvim/lua/ppechkurov/set.lua
@@ -15,6 +15,7 @@ vim.opt.backup = false
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
 vim.opt.updatetime = 100
