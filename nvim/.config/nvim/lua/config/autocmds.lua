b307f6038a7c1bbdc355e3b61a868980152a83e5
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-02-05 21:36:53 +0300

    update sfdx commands

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function sfdx(args)
+  vim.fn.jobstart(args, {
+    on_stdout = function(id, data)
+      print(vim.fn.join(data))
+    end,
+    on_stderr = function(id, data)
+      print(vim.fn.join(data))
+    end,
+    on_exit = function() end,
+  })
+end
+
+vim.api.nvim_create_user_command("Deploy", function()
+  sfdx("sfdx force:source:deploy -p" .. " " .. vim.fn.expand("%"))
+end, {})
+
+vim.api.nvim_create_user_command("OpenOrg", function()
+  sfdx("sfdx force:org:open")
+end, {})
