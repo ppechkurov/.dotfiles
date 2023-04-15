de3401af24779c87f660758c0a253292fa7dd408
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-04-15 19:50:33 +0300

    finally working astro config for ts

--- astronvim/.config/astronvim/lua/user/init.lua
+++ astronvim/.config/astronvim/lua/user/init.lua
@@ -1,11 +1,4 @@
 for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
 	vim.api.nvim_set_hl(0, group, {})
 end
-return {
-	lsp = {
-		formatting = {
-			format_on_save = true, -- enable or disable automatic formatting on save
-			timeout_ms = 3200, -- adjust the timeout_ms variable for formatting
-		},
-	},
-}
+return {}
