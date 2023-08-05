92487cbe4f92169a244f60287285029acc5fad28
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-08-05 17:13:39 +0300

    configure telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,10 @@
 require 'vimopts'
 require 'setup'
 
+require 'plugins.telescope.config'
+
+require 'keymaps'
+require 'autocommands'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/.config/nvim/lua/plugins/telescope/config.lua
@@ -0,0 +1,31 @@
+-- [[ Configure Telescope ]]
+-- See `:help telescope` and `:help telescope.setup()`
+local telescope = require('telescope')
+
+telescope.setup {
+	defaults = { mappings = { i = { ['<C-u>'] = false, ['<C-d>'] = false, }, } },
+}
+
+-- Enable telescope fzf native, if installed
+pcall(telescope.load_extension, 'fzf')
+
+-- [[ Keymaps ]]
+-- See `:help telescope.builtin`
+local map = vim.keymap.set
+
+map('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+map('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+map('n', '<leader>/', function()
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+map('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+map('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+map('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+map('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+map('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+map('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
