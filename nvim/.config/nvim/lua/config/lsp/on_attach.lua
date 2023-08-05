411ee26c5075cc973ae96d7673320839973ad6d3
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-08-05 21:23:11 +0300

    add statusline

--- nvim/.config/nvim/lua/config/lsp/on_attach.lua
+++ nvim/.config/nvim/lua/config/lsp/on_attach.lua
@@ -19,6 +19,7 @@ local on_attach = function(_, bufnr)
 	nmap('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction')
 
 	nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 	nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
 	nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
@@ -29,18 +30,7 @@ local on_attach = function(_, bufnr)
 	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-	-- Lesser used LSP functionality
-	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-	nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-	nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-	nmap('<leader>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, '[W]orkspace [L]ist Folders')
-
-	-- Create a command `:Format` local to the LSP buffer
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
+	nmap('<leader>f', vim.lsp.buf.format, '[F]ormat current buffer with LSP')
 end
 
 return on_attach
