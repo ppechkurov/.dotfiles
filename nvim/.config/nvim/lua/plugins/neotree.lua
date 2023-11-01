12fb7fa084f99b1ba4a64c489b4823c0586f1442
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-11-01 10:10:41 +0300

    update neotree config
    added autoclose
    move tree to the right

---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,34 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  config = function(_, opts)
+    opts.event_handlers = opts.event_handlers or {}
+    vim.list_extend(opts.event_handlers, {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          -- auto close
+          -- vimc.cmd("Neotree close")
+          -- OR
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+    })
+    require("neo-tree").setup(opts)
+  end,
+  opts = {
+    window = {
+      position = "right",
+      mappings = {
+        ["l"] = "open",
+        ["h"] = "close_node",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        always_show = {
+          ".config",
+        },
+      },
+    },
+  },
+}
