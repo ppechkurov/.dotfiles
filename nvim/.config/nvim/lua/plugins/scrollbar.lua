31102c248a923873fe77a93ee6e083100ccf50bd
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-11-02 14:24:11 +0300

    add scrollbar

---
+++ nvim/.config/nvim/lua/plugins/scrollbar.lua
@@ -0,0 +1,9 @@
+return {
+  "petertriho/nvim-scrollbar",
+  config = function()
+    require("scrollbar").setup({
+      handle = { color = "#9e9e9e" },
+      excluded_filetypes = { "alpha" },
+    })
+  end,
+}
