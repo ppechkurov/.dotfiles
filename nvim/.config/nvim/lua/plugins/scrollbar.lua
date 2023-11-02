054a5e82714711dd160077ae4a39c61fb5629100
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-11-02 15:57:16 +0300

    commit with feature

--- nvim/.config/nvim/lua/plugins/scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/scrollbar.lua
@@ -1,6 +1,7 @@
 return {
   "petertriho/nvim-scrollbar",
   config = function()
+    -- add feature
     require("scrollbar").setup({
       handle = { color = "#9e9e9e" },
       excluded_filetypes = { "alpha" },
