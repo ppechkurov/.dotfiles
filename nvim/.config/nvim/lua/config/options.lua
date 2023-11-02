e90b2d7dd72cf946f266643c259203bcc48a3449
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-11-02 14:21:10 +0300

    update plugins, add npm autocompletion, scrolloff

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.scrolloff = 8
 vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
 vim.g.neovide_transparency = 0.8
+
+vim.o.colorcolumn = "80,100,120"
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
