f2d1917e4ce7ce9cb3f5e0315195fe76a3cd990b
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-11-07 12:20:50 +0300

    resolve relative import issue

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -5,6 +5,12 @@ return {
     servers = {
       tsserver = {
         root_dir = require("lspconfig.util").root_pattern(".git"),
+        init_options = {
+          preferences = {
+            importModuleSpecifierPreference = "relative", -- relative path import
+            -- importModuleSpecifierEnding = "minimal", -- remove .js from import
+          },
+        },
       },
     },
   },
