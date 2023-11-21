9351bdf681de540a5cf88e7d33d025f0bb760b70
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-11-21 08:36:54 +0300

    add hardtime and schemastore

---
+++ nvim/.config/nvim/lua/plugins/schemastore.lua
@@ -0,0 +1,19 @@
+return {
+  "b0o/schemastore.nvim",
+  config = function()
+    require("lspconfig").yamlls.setup({
+      settings = {
+        yaml = {
+          schemaStore = {
+            -- You must disable built-in schemaStore support if you want to use
+            -- this plugin and its advanced options like `ignore`.
+            enable = false,
+            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+            url = "",
+          },
+          schemas = require("schemastore").yaml.schemas(),
+        },
+      },
+    })
+  end,
+}
