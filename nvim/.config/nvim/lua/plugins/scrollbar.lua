5e6a1c5dcc5421e5b1daf82f60289799fe23a644
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-12-27 09:57:41 +0300

    update scrollbar excluded filetypes

--- nvim/.config/nvim/lua/plugins/scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/scrollbar.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require("scrollbar").setup({
       handle = { color = "#9e9e9e" },
-      excluded_filetypes = { "alpha" },
+      excluded_filetypes = { "alpha", "noice", "cmp_docs", "cmp_menu", "prompt", "TelescopePrompt" },
     })
   end,
 }
