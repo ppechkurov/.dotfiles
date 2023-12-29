4215ba81e89f8bf3dcd74e41dc9cd485e8ca5943
Petr Pechkurov <petr.pechkurov@gmail.com>
2023-12-29 13:14:22 +0300

    update keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- console.log variable under the cursor
-vim.keymap.set("n", "<leader>cll", "yiwoconsole.log('<c-r>\":', <c-r>\");<esc>_", { silent = true })
+vim.keymap.set(
+  "n",
+  "<leader>cll",
+  "yiwoconsole.log('<c-r>\":', <c-r>\");<esc>_",
+  { silent = true, desc = "Console: Log variable under cursor" }
+)
+
+vim.keymap.set("n", "n", "nzz", { silent = true, desc = "Center screen after n" })
+vim.keymap.set("n", "N", "Nzz", { silent = true, desc = "Center screen after N" })
