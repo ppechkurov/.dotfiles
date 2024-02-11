5af6206214736fe3fb860ead81d0632a35976872
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-02-11 17:45:11 +0300

    add indentation guides

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,13 +1,12 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 vim.keymap.set(
-  "n",
-  "<leader>cll",
-  "yiwoconsole.log('<c-r>\":', <c-r>\");<esc>_",
-  { silent = true, desc = "Console: Log variable under cursor" }
+  'n',
+  '<leader>cll',
+  'yiwoconsole.log(\'<c-r>":\', <c-r>");<esc>_',
+  { silent = true, desc = 'Console: Log variable under cursor' }
 )
 
-vim.keymap.set("n", "n", "nzz", { silent = true, desc = "Center screen after n" })
-vim.keymap.set("n", "N", "Nzz", { silent = true, desc = "Center screen after N" })
-
+vim.keymap.set('n', 'n', 'nzz', { silent = true, desc = 'Center screen after n' })
+vim.keymap.set('n', 'N', 'Nzz', { silent = true, desc = 'Center screen after N' })
--- nvim/.config/nvim/lua/plugins/ui/ufo.lua
+++ nvim/.config/nvim/lua/plugins/ui/ufo.lua
@@ -20,13 +20,10 @@ return {
 
     vim.o.foldcolumn = '1' -- '0' is not bad
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-    -- vim.o.foldlevelstart = 99
+    vim.o.foldlevelstart = -1
+    vim.o.foldmethod = 'indent'
     vim.o.foldenable = true
     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-
-    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
     local handler = function(virtText, lnum, endLnum, width, truncate)
       local newVirtText = {}
@@ -57,22 +54,22 @@ return {
     end
 
     local ftMap = {
-      -- typescriptreact = { "lsp", "treesitter" },
-      -- python = { "indent" },
-      -- git = "",
+      typescript = { 'lsp', 'treesitter' },
+      python = { 'indent' },
+      git = '',
     }
 
     require('ufo').setup({
       fold_virt_text_handler = handler,
-      open_fold_hl_timeout = 10,
-      enable_get_fold_virt_text = true,
+      -- open_fold_hl_timeout = 10,
+      -- enable_get_fold_virt_text = true,
       close_fold_kinds = {},
       -- close_fold_kinds = { "imports", "comment" },
       provider_selector = function(bufnr, filetype, buftype)
         -- if you prefer treesitter provider rather than lsp,
         -- return ftMap[filetype] or {'treesitter', 'indent'}
         return ftMap[filetype]
-        -- return { "treesitter", "indent" }
+        -- return { 'treesitter', 'indent' }
 
         -- refer to ./doc/example.lua for detail
       end,
@@ -92,10 +89,14 @@ return {
       },
     })
 
+    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-    vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
-    vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+
+    -- vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+    -- vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+    -- vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+    -- vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
     vim.keymap.set('n', 'zK', function()
       local winid = require('ufo').peekFoldedLinesUnderCursor()
       if not winid then
