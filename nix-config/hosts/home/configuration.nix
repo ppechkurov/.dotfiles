2a013fc22bbaf5b0d3d2b4c6be35bc5e3e806ab0
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-04-27 20:15:17 +0300

    add wallpaper

--- nix-config/hosts/home/configuration.nix
+++ nix-config/hosts/home/configuration.nix
@@ -3,7 +3,13 @@
 
   # declare hostname
   config.networking.hostName = "home";
-  config.monitor = { "Virtual-1" = { mode = "1680x1050@59.954Hz"; }; };
+
+  config.monitor = {
+    "Virtual-1" = {
+      mode = "1680x1050@59.954Hz";
+      bg = "hackerman-wallpapers.jpg fill";
+    };
+  };
 
   config.home-manager = {
     users.${config.username} = import ./home.nix;
--- nix-config/hosts/home/home.nix
+++ nix-config/hosts/home/home.nix
@@ -1,9 +1,9 @@
 {
   imports = [ ../../modules/home-manager ];
-  # wayland.windowManager.sway.settings.config.output = {
-  #   "Virtual-1" = {
-  #     # mode = "1920x1080@59.963Hz";
-  #     mode = "1680x1050@59.954Hz";
-  #   };
-  # };
+
+  xdg.configFile.sway = {
+    enable = true;
+    source = ./hackerman-wallpapers.jpg;
+    target = "sway/hackerman-wallpapers.jpg";
+  };
 }
