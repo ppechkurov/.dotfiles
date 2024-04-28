631039b0ddc84a7f3f9265a0935baa2410cc3080
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-04-29 00:53:47 +0300

    working codeium

--- nix-config/modules/home-manager/programs/nvim/config/lua/plugins/codeium.lua
+++ nix-config/modules/home-manager/programs/nvim/config/lua/plugins/codeium.lua
@@ -1,7 +1,5 @@
 return {
-  'Exafunction/codeium.nvim',
+  'Exafunction/codeium.vim',
   event = 'BufEnter',
-  config = function()
-    require('codeium').setup({})
-  end,
+  -- ln -s "$(which codeium_language_server)" /home/petrp/.local/share/.codeium/bin/f772d3d7b45d3f1aea82cee1fb50501c8869f1a2/language_server_linux_x64
 }
