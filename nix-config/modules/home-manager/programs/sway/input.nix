fab3041718d507783a2af0d904ae78f9b1ecf2cc
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-05-02 17:21:36 +0300

    update keyboard ru layout

--- nix-config/modules/home-manager/programs/sway/input.nix
+++ nix-config/modules/home-manager/programs/sway/input.nix
@@ -1,7 +1,7 @@
 {
   "type:keyboard" = {
     xkb_layout = "us,ru";
-    xkb_variant = "real-prog-dvorak,programmer_ru";
+    xkb_variant = "real-prog-dvorak,";
     xkb_options = "grp:alt_shift_toggle,caps:escape"; # switch layout
     repeat_delay = "250";
     repeat_rate = "45";
