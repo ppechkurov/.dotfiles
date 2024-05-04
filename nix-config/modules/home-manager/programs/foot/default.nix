844142d705c81bfffda1eb929de0b2992247e42b
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-05-04 14:02:40 +0300

    add vim keys to foot

--- nix-config/modules/home-manager/programs/foot/default.nix
+++ nix-config/modules/home-manager/programs/foot/default.nix
@@ -7,6 +7,16 @@
         main = { font = "JetBrainsMono Nerd Font:size=16"; };
         mouse = { hide-when-typing = "yes"; };
         cursor = { blink = "yes"; };
+        key-bindings = {
+          scrollback-up-half-page = "Control+k";
+          scrollback-down-half-page = "Control+j";
+          show-urls-launch = "Control+u";
+          search-start = "Control+slash";
+        };
+        search-bindings = {
+          find-prev = "Control+n";
+          find-next = "Control+Shift+n";
+        };
       };
     };
   };
