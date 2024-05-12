e8a3956e5065a8e309856ffb856503cea298ae2f
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-05-12 10:23:49 +0300

    add printing for home

--- nix-config/hosts/home/configuration.nix
+++ nix-config/hosts/home/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, config, ... }: {
+{ inputs, config, pkgs, ... }: {
   imports = [ ../../modules/nixos/common ./hardware-configuration.nix ];
 
   # declare hostname
@@ -8,6 +8,9 @@
     "Virtual-1" = { mode = "1680x1050@59.954Hz"; };
     "*" = { bg = "hackerman-wallpapers.jpg fill"; };
   };
+
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ canon-cups-ufr2 gutenprint ];
 
   home-manager = {
     users.${config.username} = import ./home.nix;
