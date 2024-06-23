6d0f87d5d1183ae71e34ee864a6d92f08c9e00ff
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-06-23 18:24:39 +0300

    update home workspaces

---
+++ nix-config/hosts/home/hyprland/workspaces.nix
@@ -0,0 +1,18 @@
+{
+  wayland.windowManager.hyprland.settings.workspace = [
+    # left
+    "1, monitor:DVI-D-1, default:true"
+    "2, monitor:DVI-D-1"
+    "3, monitor:DVI-D-1"
+    "4, monitor:DVI-D-1"
+    "5, monitor:DVI-D-1"
+
+    # right
+    "6, monitor:DP-4, default:true"
+    "7, monitor:DP-4"
+    "8, monitor:DP-4"
+    "9, monitor:DP-4"
+    "10, monitor:DP-4"
+  ];
+}
+
