addabea8ec5400bb1b31955f6032de743b016e4a
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-06-29 22:38:42 +0300

    add proton vpn

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -8,7 +8,7 @@
   };
 
   home.sessionPath = [ "$HOME/.npm-global/bin" ];
-  home.packages = with pkgs; [ zoom-us ansible obsidian just ];
+  home.packages = with pkgs; [ zoom-us ansible obsidian just protonvpn-gui ];
 
   local.zellij.enable = true;
   local.mysql.enable = true;
