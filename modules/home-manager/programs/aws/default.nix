d93737db195850b7411bdd23e42af2994f5eb8db
Petr Pechkurov <petr.pechkurov@gmail.com>
2024-08-14 19:00:37 +0300

    update aws cli creds

--- modules/home-manager/programs/aws/default.nix
+++ modules/home-manager/programs/aws/default.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }: {
   programs.awscli.enable = true;
   programs.awscli = {
-    credentials.default.credential_process = "${pkgs.pass}/bin/pass aws/cli";
     settings.default = { region = "us-east-2"; };
+    credentials.default.credential_process =
+      "${pkgs.pass}/bin/pass aws/cli/default";
+
+    credentials.cargill.credential_process =
+      "${pkgs.pass}/bin/pass aws/cli/cargill";
   };
 }
 
