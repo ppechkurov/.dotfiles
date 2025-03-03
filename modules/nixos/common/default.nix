9011223cf56451cc3f5872aa3ed39764cf1dae3c
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-03-03 16:52:49 +0100

    update flake

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -31,7 +31,7 @@ with lib; {
     systemd.extraConfig = "DefaulTimeoutStopSec=10s";
 
     # Set your time zone.
-    time.timeZone = lib.mkDefault "Europe/Minsk";
+    time.timeZone = lib.mkDefault "Europe/Warsaw";
 
     # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
