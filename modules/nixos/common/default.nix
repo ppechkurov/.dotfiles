6edc309453622b63af80154b15b2e5bd42b9c4d6
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-03-19 07:11:06 +0100

    add flameshot wayland support

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -150,7 +150,7 @@ with lib; {
       alsa-utils
       curl
       docker-credential-helpers
-      flameshot
+      (flameshot.override { enableWlrSupport = true; })
       git
       grim
       jellyfin-ffmpeg
