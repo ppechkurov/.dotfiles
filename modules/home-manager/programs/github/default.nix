2bf33cd7fbad012fa3afd9de7bc3e37c7be60b40
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-04-28 16:51:05 +0200

    finalyze neovim config

--- modules/home-manager/programs/github/default.nix
+++ modules/home-manager/programs/github/default.nix
@@ -1,8 +1,7 @@
-{ pkgs-unstable, ... }: {
+{ ... }: {
   config = {
     programs.gh = {
       enable = true;
-      extensions = [ pkgs-unstable.gh-dash ];
       settings = {
         editor = "nvim";
         git_protocol = "ssh";
