95e1e5975e5c56df7387be12960c3a67764497de
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-05-17 19:05:08 +0300

    refactor peer interface creation function

--- modules/nixos/wireguard/peer.nix
+++ modules/nixos/wireguard/peer.nix
@@ -8,16 +8,13 @@ let
   server = globals.wg.server;
 
   # Function to generate WireGuard interface configuration
-  mkInterface = allowedIPs: address: listenPort: {
+  mkInterface = { address, allowedIPs, endpoint }: {
     inherit address;
     privateKeyFile = config.age.secrets."${privateKeyFilename}".path;
-    peers = with server;
-      let publicIpv4 = cfg.server.publicIpv4;
-      in [{
-        inherit publicKey allowedIPs;
-        endpoint = "${publicIpv4}:${toString listenPort}";
-        persistentKeepalive = 15;
-      }];
+    peers = with server; [{
+      inherit publicKey allowedIPs endpoint;
+      persistentKeepalive = 15;
+    }];
   };
 in {
   config = lib.mkIf cfg.enable {
@@ -26,14 +23,19 @@ in {
     networking.firewall.trustedInterfaces = [ "tun" ];
     networking.firewall.interfaces.vpn.allowedTCPPorts = [ 8080 ];
 
-    networking.wg-quick.interfaces = {
-      vpn = mkInterface [ "0.0.0.0/0" "::/0" ]
-        (with peer.networks; [ "${vpn.ipv4}/24" "${vpn.ipv6}" ]) listenPort;
+    networking.wg-quick.interfaces = let serverPublicIp = cfg.server.publicIpv4;
+    in {
+      vpn = mkInterface {
+        address = (with peer.networks; [ "${vpn.ipv4}/24" "${vpn.ipv6}/64" ]);
+        allowedIPs = [ "0.0.0.0/0" "::/0" ];
+        endpoint = "${serverPublicIp}:${toString listenPort}";
+      };
 
-      tun =
-        mkInterface (with server.networks.tun; [ "${ipv4}/24" "${ipv6}/64" ])
-        (with peer.networks; [ "${tun.ipv4}/24" "${tun.ipv6}" ])
-        (listenPort + 1);
+      tun = mkInterface {
+        address = (with peer.networks; [ "${tun.ipv4}/24" "${tun.ipv6}/64" ]);
+        allowedIPs = (with server.networks.tun; [ "${ipv4}/24" "${ipv6}/64" ]);
+        endpoint = "${serverPublicIp}:${toString (listenPort + 1)}";
+      };
     };
   };
 }
