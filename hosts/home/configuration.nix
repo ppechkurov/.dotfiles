5dfe50266706f4dadf253d1b4a07d3ddbb05d54f
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-07-06 15:13:51 +0300

    add jellyfin media player to home

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -55,7 +55,7 @@
 
   environment.systemPackages = with pkgs;
     let gostman = inputs.gostman.packages.${pkgs.system}.default;
-    in [ steam-run protonup gostman ];
+    in [ steam-run protonup gostman jellyfin-media-player ];
 
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
