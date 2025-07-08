9033818c265827450cb5d5b11023afe169a2a962
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-07-08 09:21:38 +0300

    add cache substituters

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -23,7 +23,12 @@ in with lib; {
       settings = {
         experimental-features = [ "nix-command" "flakes" ];
         auto-optimise-store = true;
-        trusted-users = [ "root" "petrp" ];
+        trusted-substituters =
+          [ "https://cache.nixos.org/" "https://ppechkurov.cachix.org" ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "ppechkurov.cachix.org-1:ChzUYtQ6adSICkzYQ9LznJpeIm/a2oeQh3SVjXXZnPg="
+        ];
       };
       gc = {
         automatic = true;
