bc1ce81170d0cf1e037790e6672fb11a60a41f21
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-07-08 16:38:24 +0200

    add ghostty hm module

---
+++ modules/home-manager/programs/ghostty/default.nix
@@ -0,0 +1,17 @@
+{ inputs, pkgs, ... }:
+let ghostty = inputs.ghostty.packages.${pkgs.system}.default;
+in {
+  programs.ghostty.enable = true;
+  programs.ghostty = {
+    package = ghostty;
+    enableZshIntegration = true;
+    settings = { theme = "GruvboxDard"; };
+  };
+
+  nix.settings = {
+    trusted-substituters = [ "https://ghostty.cachix.org" ];
+    trusted-public-keys =
+      [ "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns=" ];
+  };
+}
+
