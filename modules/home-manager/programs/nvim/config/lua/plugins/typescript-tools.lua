49394ab14202f66cbcc94068c7e0b436e66daf0a
Petr Pechkurov <petr.pechkurov@gmail.com>
2025-10-17 18:30:52 +0200

    Update work

--- modules/home-manager/programs/nvim/config/lua/plugins/typescript-tools.lua
+++ modules/home-manager/programs/nvim/config/lua/plugins/typescript-tools.lua
@@ -1,7 +1,7 @@
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  enabled = false,
+  enabled = true,
   opts = {
     root_dir = require('lspconfig.util').root_pattern('.git'),
     on_attach = function()
