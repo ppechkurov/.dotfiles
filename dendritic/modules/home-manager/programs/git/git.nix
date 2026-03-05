608ea526ebf37f3481884ecaa7928a1718895481
Petr Pechkurov <petr.pechkurov@gmail.com>
2026-03-06 00:12:36 +0300

    Add nvim codediff plugin

--- dendritic/modules/home-manager/programs/git/git.nix
+++ dendritic/modules/home-manager/programs/git/git.nix
@@ -2,15 +2,20 @@
 let email = self.globals.emails.gmail;
 in {
   flake.modules.homeManager.git = { pkgs, lib, ... }: {
-    programs.git.enable = true;
     home.packages = [ pkgs.lazygit ];
 
+    programs.git.enable = true;
     programs.git.settings = {
       user.email = email;
       user.name = "Petr Pechkurov";
       init.defaultBranch = "main";
       credential.helper = "store";
       push.autosetupremote = true;
+      merge.tool = "codediff";
+      diff.tool = "codediff";
+      difftool.codediff.cmd = ''
+        nvim "$LOCAL" "$REMOTE" +"CodeDiff file $LOCAL $REMOTE"
+      '';
     };
   };
 }
