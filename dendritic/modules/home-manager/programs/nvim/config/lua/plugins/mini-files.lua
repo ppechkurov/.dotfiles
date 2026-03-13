db9790a031f9d3ebde0b25ed90f8a7ee30d7bf2d
Petr Pechkurov <petr.pechkurov@gmail.com>
2026-03-13 19:49:11 +0300

    Add mini-files for testing

--- dendritic/modules/home-manager/programs/nvim/config/lua/plugins/mini-files.lua
+++ dendritic/modules/home-manager/programs/nvim/config/lua/plugins/mini-files.lua
@@ -1,6 +1,11 @@
 return {
   'nvim-mini/mini.files',
   config = function()
+    local wk = require('which-key')
+    wk.register({
+      ['<leader>e'] = { ':lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>', 'File Tree' },
+      ['<BS>'] = { ':lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>', 'File Tree' },
+    })
     require('mini.files').setup()
   end,
 }
