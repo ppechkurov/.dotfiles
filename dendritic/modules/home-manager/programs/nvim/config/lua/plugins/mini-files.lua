aa2ae7efb111ec75ad2a03ca4e4e2aa52086f773
Petr Pechkurov <petr.pechkurov@gmail.com>
2026-04-17 07:42:36 +0000

    Update codediff plugin

--- dendritic/modules/home-manager/programs/nvim/config/lua/plugins/mini-files.lua
+++ dendritic/modules/home-manager/programs/nvim/config/lua/plugins/mini-files.lua
@@ -4,7 +4,6 @@ return {
     local wk = require('which-key')
     wk.register({
       ['<leader>e'] = { ':lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>', 'File Tree' },
-      ['<BS>'] = { ':lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>', 'File Tree' },
     })
     require('mini.files').setup()
   end,
