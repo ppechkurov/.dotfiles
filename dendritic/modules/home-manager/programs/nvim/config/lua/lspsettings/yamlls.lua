c1c5904134ac05c2e10ad0ddc9fb83da8e87e4e1
Petr Pechkurov <petr.pechkurov@gmail.com>
2026-04-22 08:39:58 +0300

    Change work petrp ssh key

--- dendritic/modules/home-manager/programs/nvim/config/lua/lspsettings/yamlls.lua
+++ dendritic/modules/home-manager/programs/nvim/config/lua/lspsettings/yamlls.lua
@@ -5,17 +5,18 @@ return {
         -- You must disable built-in schemaStore support if you want to use
         -- this plugin and its advanced options like `ignore`.
         enable = false,
-        validate = false,
+        validate = true,
         -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-        -- url = '',
+        url = '',
       },
-      schemas = require('schemastore').yaml.schemas(),
-      -- schemas = {
-      --   kubernetes = 'k8s-*.{yml,yaml}',
-      --   ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
-      --   ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
-      --   ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
-      -- },
+      -- schemas = require('schemastore').yaml.schemas(),
+      schemas = {
+        ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
+        -- ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+        ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+        kubernetes = '*.yaml',
+      },
+      format = { enable = true },
     },
   },
 }
