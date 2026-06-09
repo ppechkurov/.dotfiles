fa351157f1cede1621334e4f6b618885be45bbda
Petr Pechkurov <petr.pechkurov@gmail.com>
2026-06-10 01:08:42 +0300

    Update lexyai

--- dendritic/hosts/mini/services/lexyai.nix
+++ dendritic/hosts/mini/services/lexyai.nix
@@ -1,5 +1,10 @@
 { self, ... }: {
   flake.modules.nixos.mini = { config, ... }: {
     services.lexyai.host = self.globals.wg.peers.mini.interfaces.tun.ip;
+
+    systemd.services.lexyai = {
+      after = [ "wg-quick-tun.service" ];
+      requires = [ "wg-quick-tun.service" ];
+    };
   };
 }
